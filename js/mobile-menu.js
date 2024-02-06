2237de15cf961e801fadb760d4cf755e90a5f7bb
rv`sdev <vidziok@gmail.com>
2024-02-07 01:28:27 +0200

    Update mobile-menu.js
    listener for all buttons with class=".menu-btn-close"

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,10 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
-const menuBtnClose = document.querySelector(".menu-btn-close");
+const menuBtnClose = document.querySelectorAll(".menu-btn-close");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
-
 menuBtnOpen.addEventListener("click", toggleMenu);
-menuBtnClose.addEventListener("click", toggleMenu);
+
+menuBtnClose.forEach((item) => {
+  item.addEventListener("click", toggleMenu);
+});
